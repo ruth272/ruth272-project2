6d8cc86885c6d0b4efed0cce1f6e9a8240d3765a
Ruth Teshome <ruthteshome@Ruths-MacBook-Air.local>
2024-11-17 20:55:04 -0500

    defining conctructors

--- Client.java
+++ Client.java
@@ -1,8 +1,17 @@
-public class Client{
-    //constructor
-    public Client() {
-        //serverSocket
+import java.util.*;
+import java.net.*;
+import java.io.*;
 
+public class Client{
+    public Socket socket;
+    public PrintWriter output;
+    public BufferedReader input;
+
+    //constructor
+    public Client(String host, int port) {
+        this.socket = new Socket(host,port);
+        this.output = new PrintWriter(socket.getOutputStream());
+        this.input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
     }
 
     public class handshake() {
--- Server.java
+++ Server.java
@@ -1,7 +1,15 @@
-public class Server{
-    //constructor
-    public Server() {
+import java.util.*;
+import java.net.*;
+import java.io.*;
 
+public class Server{
+    public ServerSocket serverSocket;
+    public int port;
+
+    //constructor
+    public Server(int port) {
+        this.port = port;
+        this.serverSocket = new ServerSocket(port);
     }
 
     public class serve() {
