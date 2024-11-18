fa7780c2db2a92cc880878f3a7f8bda5fb1ecb23
Ruth Teshome <ruthteshome@Ruths-MacBook-Air.local>
2024-11-17 20:04:10 -0500

    setting up client and server methods

--- Client.java
+++ Client.java
@@ -1,1 +1,24 @@
+public class Client{
+    //constructor
+    public Client() {
+        //serverSocket
 
+    }
+
+    public class handshake() {
+
+    }
+
+    public class request() {
+
+    }
+
+    public class disconnect() {
+
+    }
+
+    public class getSocket() {
+
+    }
+
+}
--- Server.java
+++ Server.java
@@ -1,1 +1,18 @@
+public class Server{
+    //constructor
+    public Server() {
 
+    }
+
+    public class serve() {
+
+    }
+
+    public class getConnectedTimes() {
+
+    }
+
+    public class disconnect() {
+
+    }
+}
